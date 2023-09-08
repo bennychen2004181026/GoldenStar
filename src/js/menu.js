dbc3fca1e2971d1423e79096391c51e53b335bf9
Ben Chen <77423140+bennychen2004181026@users.noreply.github.com>
2023-09-08 18:20:45 +1000

    Fix the menu javascript

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Tab navigation logic
     const tabs = document.querySelectorAll(".menu-tabs li");
     const tabContents = document.querySelectorAll(".tab-content");
-  
+    
     tabs.forEach(function(tab, tabIndex) {
       tab.addEventListener("click", function() {
         tabs.forEach(function(innerTab) {
           innerTab.classList.remove("active");
         });
         tab.classList.add("active");
-  
+    
         tabContents.forEach(function(content, contentIndex) {
           if (contentIndex === tabIndex) {
             content.classList.add("active");
@@ -18,62 +19,81 @@ document.addEventListener("DOMContentLoaded", function() {
         });
       });
     });
-  });
-
-  //open and close the cart:
-  function openCart() {
-    document.getElementById("cartSidebar").style.right = "0";
-  }
   
-  function closeCart() {
-    document.getElementById("cartSidebar").style.right = "-400px";
-  }
+    // Open and close cart logic
+    function openCart() {
+      document.getElementById("cartSidebar").style.right = "0";
+    }
+    
+    function closeCart() {
+      document.getElementById("cartSidebar").style.right = "-400px";
+    }
   
-  //add items to the cart:
-  let cart = [];
-let total = 0;
-
-function addToCart(dish, price) {
-  cart.push({ dish, price });
-  total += price;
-  updateCart();
-  showToast();
-}
-
-function updateCart() {
-  let cartItemsDiv = document.querySelector('.cart-items');
-  cartItemsDiv.innerHTML = "";
+    // Cart and Toast logic
+    let cart = [];
+    let total = 0;
   
-  cart.forEach((item, index) => {
-    cartItemsDiv.innerHTML += `<div class="cart-item">${item.dish} - $${item.price.toFixed(2)}</div>`;
-  });
+    function addToCart(dish, price, action) {
+      let itemIndex = cart.findIndex(item => item.dish === dish);
   
-  document.getElementById("cartTotal").innerText = total.toFixed(2);
-  openCart();
-}
-//Attach the addToCart function to your "Add to Cart" button. For this example, let's assume the seafood dish is $12.00:
-document.addEventListener("DOMContentLoaded", function() {
+      if (action === 'add') {
+        if (itemIndex >= 0) {
+          cart[itemIndex].quantity++;
+          cart[itemIndex].price += price;
+        } else {
+          cart.push({ dish, price, quantity: 1 });
+        }
+        total += price;
+        showToast('Added to cart');
+      } else if (action === 'remove' && itemIndex >= 0) {
+        if (cart[itemIndex].quantity > 1) {
+          cart[itemIndex].quantity--;
+          cart[itemIndex].price -= price;
+        } else {
+          cart.splice(itemIndex, 1);
+        }
+        total -= price;
+        showToast('Removed from cart');
+      }
+  
+      updateCart();
+    }
+  
+    function updateCart() {
+      let cartItemsDiv = document.querySelector('.cart-items');
+      cartItemsDiv.innerHTML = "";
+      
+      cart.forEach((item, index) => {
+        cartItemsDiv.innerHTML += `<div class="cart-item">${item.dish} x${item.quantity} - $${item.price.toFixed(2)}</div>`;
+      });
+      
+      document.getElementById("cartTotal").innerText = total.toFixed(2);
+      openCart();
+    }
+  
+    // Toast function
+    function showToast(message) {
+      const toast = document.getElementById("toast");
+      toast.textContent = message;
+      toast.classList.remove("toast-hidden");
+      toast.classList.add("toast-visible");
+    
+      setTimeout(() => {
+        toast.classList.remove("toast-visible");
+        toast.classList.add("toast-hidden");
+      }, 500);
+    }
+  
+    // Attach addToCart for initial Add to Cart click
     const addToCartButtons = document.querySelectorAll(".add-to-cart");
     
     addToCartButtons.forEach(button => {
       button.addEventListener("click", function() {
-        addToCart(this.getAttribute("data-dish"), 12);  // Here 12 is the price, you can dynamically populate this as needed
+        addToCart(this.getAttribute("data-dish"), 12, 'add');  // Here 12 is the price
       });
     });
-  });
-
-  function showToast() {
-    const toast = document.getElementById("toast");
-    toast.classList.remove("toast-hidden");
-    toast.classList.add("toast-visible");
   
-    setTimeout(() => {
-      toast.classList.remove("toast-visible");
-      toast.classList.add("toast-hidden");
-    }, 500);
-  }
-
-  document.addEventListener("DOMContentLoaded", () => {
+    // Increment and Decrement buttons
     const incrementButtons = document.querySelectorAll(".increment");
     const decrementButtons = document.querySelectorAll(".decrement");
     
@@ -93,7 +113,4 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   });
-  
-
-  
   
